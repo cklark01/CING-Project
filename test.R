0da3fcc3168aa573f215291a277876517dd5f7fd
cklark01 <63281571+cklark01@users.noreply.github.com>
2022-06-30 14:40:14 +0200

    Updated Network

--- test.R
+++ test.R
@@ -14,40 +14,22 @@ styles <- c("",
             "simple"      = file.path(demo.directory, "smallDemoStyle.json"),
             "galFiltered" = file.path(demo.directory, "galFiltered-style.json"))
 
-networks <- c("",
-              "simple"      = file.path(demo.directory, "stringinteractions.cyjs"),
-              "galFiltered" = file.path(demo.directory, "galFiltered.cyjs"))
+networks <- c(
+              "simple"      = file.path(demo.directory, "stringinteractions.cyjs")
+              )
 
 
 #----------------------------------------------------------------------------------------------------
 graph.json.filename <- "stringinteractions.cyjs"
-style.json.filename <- "/Users/constantinosclark/Desktop/CING/CING-Project/cyjShiny/inst/demos/fromCytoscapeDesktop/simple/smallDemoStyle.json"
+style.json.filename <- "NetworkFiles/smallDemoStyle.json"
 #----------------------------------------------------------------------------------------------------
 
-
-
-
-mymtcars = mtcars
-mymtcars$id = 1:nrow(mtcars)
-#----------------------------------------------------------------------------------------------------
 ui = shinyUI(fluidPage(
+  tags$style("#cyjShiny{height:100vh !important;}"),
   dashboardPage(
     #these are random things i found and added maybe we can customize them later
-    dashboardHeader(title = 'Network Library', 
-                    
-                    dropdownMenu(
-                      type = 'messages', 
-                      messageItem(from = 'Mike', message = 'This is a sample message')
-                    ), #static example
-                    dropdownMenuOutput('messageMenu'), #dynamic example
-                    dropdownMenu(
-                      type = 'notifications', 
-                      notificationItem(text = 'This is a test notification')
-                    ),
-                    dropdownMenu(
-                      type = 'tasks', 
-                      taskItem(text='This is a test task', value = 50)
-                    )), 
+    dashboardHeader(title = 'Network Library'
+                ), 
     dashboardSidebar(
       sidebarMenu(
         menuItem(text='Networks', tabName = 'networks',{menuItem(text='View', tabName = 'viewer')}, {menuItem(text='Analyze', tabName = 'analyzer')}), 
@@ -55,9 +37,20 @@ ui = shinyUI(fluidPage(
       )
     ),
     dashboardBody(
-      cyjShinyOutput('cyjShiny'),
-      width=10,
-      DT::dataTableOutput('mytable'),
+      fluidRow(
+        box(cyjShinyOutput('cyjShiny')),
+        box(
+          title = "Box content here", br(),status = "primary",solidHeader = TRUE, "More box content",
+          sliderInput("slider", "Slider input:", 1, 100, 50),
+          textInput("text", "Text input:")
+        ),
+        box(
+          title = "Network Creation Protocol",status = "warning",solidHeader = TRUE, br(), "More box content",
+          sliderInput("slider", "Slider input:", 1, 100, 50),
+          textInput("text", "Text input:")
+        )
+      
+      )
     )
   
 )
@@ -71,28 +64,8 @@ server = function(input, output, session)
     graphAsJSON <- readAndStandardizeJSONNetworkFile(graph.json.filename)
     cyjShiny(graph=graphAsJSON, layoutName="preset", styleFile=style.json.filename)
   })
+
   
-  
-  output$mytable = DT::renderDataTable({
-    addCheckboxButtons <- paste0('<input type="checkbox" name="row', mymtcars$id, '" value="', mymtcars$id, '">',"")
-    #Display table with checkbox buttons
-    DT::datatable(cbind(Pick=addCheckboxButtons, mymtcars[, input$show_vars, drop=FALSE]),
-                  options = list(orderClasses = TRUE,
-                                 lengthMenu = c(5, 25, 50),
-                                 pageLength = 25, 
-                                 callback = JS("function(table) {
-    table.on('change.dt', 'tr td input:checkbox', function() {
-          setTimeout(function () {
-          Shiny.onInputChange('rows', $(this).add('tr td input:checkbox:checked').parent().siblings(':last-child').map(function() {
-          return $(this).text();
-          }).get())
-          }, 10); 
-          });
-          }")),escape = FALSE,
-                  
-    )
-  } 
-  )
   
   
 } # server
