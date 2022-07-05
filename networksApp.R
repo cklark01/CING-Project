1e1977ba20e22350893f686269d42cd8aaf67827
KyraCh <92988384+KyraCh@users.noreply.github.com>
2022-07-05 17:06:35 +0300

    Update networksApp.R

--- networksApp.R
+++ networksApp.R
@@ -1,13 +1,25 @@
-
-
 library(shiny)
 library(DT)
+library(cyjShiny)
+library(htmlwidgets)
+library(graph)
+library(jsonlite)
+library(later)
 library(shinydashboard)
 
-## module UI
-module_ui  <- function(id){
-  ns <- NS(id)
+
+data_one <- read.csv('/Users/KyriakiCH1/Desktop/lets see/data.csv')
+
+
+style.json.filename <- "/Users/KyriakiCH1/Desktop/styles.json"
+
+
+
+
+ui <-
+  
   dashboardPage(
+    
     #these are random things i found and added maybe we can customize them later
     dashboardHeader(
       title = 'NetHub'),
@@ -16,9 +28,9 @@ module_ui  <- function(id){
         sidebarSearchForm(textId = "searchText", buttonId = "searchButton",
                           label = "Search..."),
         menuItem(text='Views',icon = icon("hive"), tabName = 'views',
-                 {menuItem(text='Views in Uniprot', tabName = 'viewerU')},
-                 {menuItem(text='Views in String', tabName = 'viewerS')},
-                 {menuItem(text='Views in DisGNet', tabName = 'viewerD')}), 
+                 {menuItem(text='View in Uniprot', tabName = 'viewerU')},
+                 {menuItem(text='View in String', tabName = 'viewerS')},
+                 {menuItem(text='View in DisGNet', tabName = 'viewerD')}), 
         menuItem(text='Browse',icon = icon("filter"), tabName = 'analyze',
                  {menuItem(text='Proteins', tabName = 'viewerS')},
                  {menuItem(text='Sequence', tabName = 'viewerS')},
@@ -28,90 +40,148 @@ module_ui  <- function(id){
                  {menuItem(text='Cancer', tabName = 'can')},
                  {menuItem(text='Cancer', tabName = 'can')}),
         sliderInput("obs", "Number of Nodes:",
-                   min = 0, max = 1000, value = 500
+                    min = 0, max = 1000, value = 500
         ),
         sliderInput("obs", "Number of Edges:",
-                   min = 0, max = 1000, value = 500
+                    min = 0, max = 1000, value = 500
         )
         
       )
     ),
     dashboardBody(
+      # cyjShinyOutput('cyjShiny'), width = 10 ,
+      # tagList(
+      #   DT::dataTableOutput(outputId = ("dt1"))
+      # ),
+      tabsetPanel(id='tabs',
+                  tabPanel("Table",  DT::dataTableOutput(outputId = ("dt1"))),
+                  tabPanel("View",cyjShinyOutput('cyjShiny'), width = 10)
+      )
       
-      tagList(
-        DT::dataTableOutput(outputId = ns("dt1"))
-        
-      )      
     )
     
-    
   )
-}
+ 
 
-## module server
-module_server <- function(input, output, session ){
-  ns = session$ns
+
+
+server <- function(input,output, session) {
   
-  
-  #Function to show inputs in table
-  shinyInput <- function(FUN, len, id, ns, ...) {
-    inputs <- character(len)
-    for (i in seq_len(len)) {
-      inputs[i] <- as.character(FUN(paste0(ns(id), i), ...))
+    
+    ns = session$ns
+    
+    #Function to show inputs in table
+    shinyInput <- function(FUN, len, id, ns, ...) {
+      inputs <- character(len)
+      for (i in seq_len(len)) {
+        inputs[i] <- as.character(FUN(paste0(ns(id), i), ...))
+      }
+      inputs
     }
-    inputs
-  }
   
   #Table Dynamic creation
-  datatable_function <- reactive({
-    tibble::tibble(
+  datatable_function<- reactive({
+    data.frame(
       Select = shinyInput(checkboxInput,
-                          2,
+                          nrow(data_one),
                           'checkboxes',
                           label = NULL,
+                          
                           ns = ns,
                           width = 1,
       ),
-      Network = c('<img src="https://cytoscape.org/cytoscape-tutorials/presentations/modules/network-visualization/data-mapping-6.png" height="150"></img>','<img src="https://cytoscape.org/cytoscape-tutorials/protocols/rna-seq-data-analysis/string-up-viz.png" height="150"></img>' ), 
-      Description = c('Here is a very beautiful network', 'Another network'),
+      Network = data_one$Network ,
+      Description = (data_one$Description),
+      View = shinyInput(actionButton,
+                        nrow(data_one),
+                        'view_button',
+                        ns = ns,
+                        label = "View Network",
+                        onclick = sprintf("Shiny.setInputValue('%s', this.id)",ns("select_button"))),
+      # "No. of Nodes" = (data_one$Nodes),
+      # "No. of Edges" = (data_one$Edges),
       Download = shinyInput(downloadButton,
-                            2,
-                            'buttons',
+                            nrow(data_one),
+                            'button_',
                             ns = ns,
-                            label = "Download", 
+                            label = "Download",
                             onclick = sprintf("Shiny.setInputValue('%s', this.id)",ns("select_button")))
       
       
     )})
-  #Download Handler for all the download buttons
-  lapply(1:2 , function(i){
-    output[[paste0("buttons",i)]] <-
-      downloadHandler(
-        filename = "edgeList.txt",
-        content = function(file){
-          write.csv(datatable_function() ,file)
-        }
-      )
-  })
+  
+  my_range <- 1: nrow(data_one)
+  
+  for (i in my_range){
+    
+    lapply(my_range, function(i){
+      edgefile <- read.csv(read.csv('/Users/KyriakiCH1/Desktop/lets see/data.csv')$DownloadFile[i])
+      output[[paste0("button_",i)]] <-
+        downloadHandler(
+          filename = "edgeList.txt",
+          content = function(file){
+            write.csv(edgefile  ,file)
+          }
+        )
+      
+    })}
+  
+  
+  
+  output$dt1 <-DT::renderDataTable({ datatable(datatable_function(),
+                                               escape = FALSE,
+                                               rownames = FALSE,
+                                               selection = "single",
+                                               options = (
+                                                 list(
+                                                   preDrawCallback = JS('function() { Shiny.unbindAll(this.api().table().node()); }'),
+                                                   drawCallback = JS('function() { Shiny.bindAll(this.api().table().node()); } '),
+                                                   dom = "t",
+                                                   ordering = FALSE,
+                                                   paging = FALSE,
+                                                   autoWidth = FALSE,
+                                                   scrollY = "100vh",
+                                                   scrollCollapse = FALSE
+                                                 )
+                                               ),
+                                               callback = htmlwidgets::JS(
+                                                 "table.on('dblclick', 'td',", 
+                                                 "  function() {",
+                                                 "    var row = table.cell(this).index().row;",
+                                                
+                                                 "    Shiny.setInputValue('dt_dblclick', {dt_row: row});",
+                                                 "  }",
+                                                 ");"
+                                               )
+                                               
+                                               
+  )
+    
+  },server = FALSE)
+  
 
   
-  output$dt1 <- DT::renderDataTable({
-    datatable(datatable_function(), escape = FALSE, 
-              options = 
-                list(
-                  preDrawCallback = JS('function() { Shiny.unbindAll(this.api().table().node()); }'),
-                  drawCallback = JS('function() { Shiny.bindAll(this.api().table().node()); } ')
-                )
-    )
-  })
+    observeEvent(input$dt_dblclick, {
+
+    if (input$dt_dblclick[1] == 0) {
+    print(input$dt_dblclick)
+
+    updateTabsetPanel(session, "tabs",
+                      selected = 'View')
+
+
+    output$cyjShiny <- renderCyjShiny({
+    graphAsJSON <- readAndStandardizeJSONNetworkFile(readLines(data_one$NetworkCyto[1]))
+    cyjShiny(graph=graphAsJSON, layoutName="preset", styleFile= style.json.filename ) })
+
+
+    }})
+
+  
+ 
+    
+  
 }
 
-ui <- 
-  module_ui(id = "test_dt_inside_module")
-
-
-server <- function(input, output, session) {
-  callModule(module = module_server , id = "test_dt_inside_module")
-}
 
 shinyApp(ui, server)
