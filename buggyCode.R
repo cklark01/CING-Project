16acad4279b64f3cf2e7b6e30fd2be043afe5879
KyraCh <92988384+KyraCh@users.noreply.github.com>
2022-07-11 17:52:01 +0300

    Update buggyCode.R

--- buggyCode.R
+++ buggyCode.R
@@ -3,9 +3,19 @@ library(plyr)
 library(dplyr)
 library(DT)
 library(readxl)
+library(cyjShiny)
+library(htmlwidgets)
+library(graph)
+library(jsonlite)
+library(later)
+library(shinydashboard)
+library(tidyverse)
+library(shinyWidgets)
+
 
 # data_one <- read_excel("/Users/KyriakiCH1/Desktop/data_one.xlsx")
-data_one <- read.csv('/Users/KyriakiCH1/Desktop/lets see/data.csv')
+data_one <- read.csv('/Users/KyriakiCH1/Desktop/NetworksApp/data/data.csv')
+style.json.filename <- "/Users/KyriakiCH1/Desktop/NetworksApp/shiny/styles.json"
 
 
 # Define UI for application that draws a histogram
@@ -19,17 +29,27 @@ ui <- fluidPage(
     sidebarPanel(
       helpText("Filters"),
       
-      selectInput("pickvalue", label = "Disease", unique(data_one$Disease),
+      selectInput("databaseSelector", label = "Database", unique(data_one$Database),
                   selected = NULL, multiple = T),
-      selectInput("pickvalue2", label = "Database", unique(data_one$Database),
+      selectInput("diseaseSelector", label = "Disease", unique(data_one$Disease),
+                  selected = NULL, multiple = T),
+      selectInput("typeSelector", label = "Type", unique(data_one$Type),
                   selected = NULL, multiple = T)
       
     ),
     
     
     # Show a plot of the generated distribution
+    
     mainPanel(
-      dataTableOutput('table')
+      tabsetPanel(id='tabs',
+                  tabPanel("Table",  DT::dataTableOutput(outputId = ("table"))),
+                  tabPanel("View",cyjShinyOutput('cyjShiny'), width = 10)
+                           
+                           
+                  
+                  
+      )
     )
   )
 )
@@ -47,24 +67,29 @@ server <- function(input, output,session) {
     }
     inputs
   }
+  
+  # Function for data filtering 
   entrys <- reactive({
     
     dat <- data_one
     
-    if (!is.null(input$pickvalue)){dat <- dat %>% filter(Disease %in% input$pickvalue)} 
-    if (!is.null(input$pickvalue2)){dat <- dat %>% filter(Database %in% input$pickvalue2)}
+    if (!is.null(input$diseaseSelector)){dat <- dat %>% filter(Disease %in% input$diseaseSelector)} 
+    if (!is.null(input$databaseSelector)){dat <- dat %>% filter(Database %in% input$databaseSelector)}
+    if (!is.null(input$typeSelector)){dat <- dat %>% filter(Type %in% input$typeSelector)}
     dat <- dat %>% select(-Disease)
     
+    print(dat$DownloadFile[1])
     return(dat)
     
   })
+  
+  
   datatable_function<- reactive({
     data.frame(
       Select = shinyInput(checkboxInput,
-                          length(entrys()),
+                          nrow(entrys()),
                           'checkboxes',
                           label = NULL,
-                          
                           ns = ns,
                           width = 1,
       ),
@@ -73,17 +98,40 @@ server <- function(input, output,session) {
       # "No. of Nodes" = (data_one$Nodes),
       # "No. of Edges" = (data_one$Edges),
       Download = shinyInput(downloadButton,
-                            length(entrys()),
+                            nrow(entrys()),
                             'button_',
                             ns = ns,
                             label = "Download",
-                            onclick = sprintf("Shiny.setInputValue('%s', this.id)",ns("select_button")))
+                            onclick = sprintf("Shiny.setInputValue('%s', this.id)",ns("select_button"), downloadLoop())
+                            
+                            )
+      
       
       
     )})
   
+  # my_range <- 1: nrow(entrys())
+  downloadLoop <- reactive({
+    print(nrow(entrys()))
+    for (i in  1: nrow(entrys())){
   
-  output$table <-DT::renderDataTable({ datatable(datatable_function(),
+      lapply( 1: nrow(entrys()), function(i){
+        edgefile <- read.csv(entrys()$DownloadFile[i])
+        output[[paste0("button_",i)]] <-
+          downloadHandler(
+            filename = "edgeList.txt",
+            content = function(file){
+              write.csv(edgefile  ,file)
+            }
+          )
+  
+      })}
+  })
+  
+
+
+  
+  output$table <- DT::renderDataTable({ datatable(datatable_function(),
                                                escape = FALSE,
                                                rownames = FALSE,
                                                selection = "single",
@@ -101,17 +149,41 @@ server <- function(input, output,session) {
                                                callback = htmlwidgets::JS(
                                                  "table.on('dblclick', 'td',", 
                                                  "  function() {",
-                                                 "    var row = table.cell(this).index().row;",
+                     
+                                                  "    var row = table.cell(this).index().row;",
                                                  
                                                  "    Shiny.setInputValue('dt_dblclick',  {row});",
                                                  "  }",
                                                  ");"
                                                )
                                                
-                                               
   )
     
-  },server = FALSE) }
+  },server = FALSE)  
+  
+  observeEvent(input$dt_dblclick, {
+    
+    
+    print(input$dt_dblclick$row)
+    
+    
+    updateTabsetPanel(session, "tabs",
+                      selected = 'View')
+    
+    
+    output$cyjShiny <- renderCyjShiny({
+      graphAsJSON <- readAndStandardizeJSONNetworkFile(readLines(data_one$NetworkCyto[input$dt_dblclick$row + 1]))
+      cyjShiny(graph=graphAsJSON, layoutName="preset", styleFile=data_one$StyleFile[input$dt_dblclick$row + 1])  })
+    
+    
+    
+    
+  })
+  
+  
+  
+}
+
 
 # Run the application 
 shinyApp(ui = ui, server = server)
